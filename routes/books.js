11561035ea3fad844591c15aee5d300456fecb86
leechan <dlcks0601@gmail.com>
2024-10-04 17:06:53 +0900

    데브코스 4th 10.4 과제

--- routes/books.js
+++ routes/books.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {
+    allBooks,
+    bookDetail,
+    booksByCategory,
+} = require('../controller/BookController');
 
-// 전체 도서 조회
-router.get('/', (req, res) => {
-    res.json('전체 도서 조회');
-});
-// 개별 도서 조회
-router.post('/books/:id', (req, res) => {
-    res.json('개별 도서 조회');
-});
-// 카테고리별 도서 목록 조회
-router.post('/books', (req, res) => {
-    res.json('카테고리별 도서 목록 조회');
-});
+router.use(express.json());
+
+router.get('/', allBooks);
+router.get('/:id', bookDetail);
+router.get('/category');
 
 module.exports = router;
