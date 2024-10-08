aede29955c665fd8400b2cbac06cad1a7c47a90e
leechan <dlcks0601@gmail.com>
2024-10-08 17:55:59 +0900

    10.8 프로그래머스 과제

--- routes/likes.js
+++ routes/likes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { addLike, removeLike } = require('../controller/LikeController');
+
+router.use(express.json());
 
 // 좋아요 추가
-router.post('/likes/:id', (req, res) => {
-    res.json('좋아요 추가');
-});
+router.post('/:id', addLike);
 // 좋아요 삭제
-router.post('/likes/:id', (req, res) => {
-    res.json('좋아요 삭제');
-});
+router.delete('/:id', removeLike);
 
 module.exports = router;
