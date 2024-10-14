6bd3bc144a6f63bd9c793620212e65c2d32b33a3
leechan <dlcks0601@gmail.com>
2024-10-14 15:58:32 +0900

    10.14 월요일 orderController 비동기

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,7 +1,16 @@
-const conn = require('../mariadb'); // db 모듈
+// const conn = require('../mariadb'); // db 모듈
+const mariadb = require('mysql2/promise');
 const { StatusCodes } = require('http-status-codes'); // status code 모듈
 
-const order = (req, res) => {
+const order = async (req, res) => {
+    const conn = await mariadb.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'root',
+        database: 'Bookshop',
+        dateStrings: true,
+    });
+
     const {
         items,
         delivery,
@@ -11,24 +20,20 @@ const order = (req, res) => {
         firstBookTitle,
     } = req.body;
 
-    let delivery_id = 3;
-    let order_id = 2;
+    let delivery_id;
+    let order_id;
+
     let sql =
         'INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)';
     let values = [delivery.address, delivery.receiver, delivery.contact];
-    // conn.query(sql, values, (err, results) => {
-    //     if (err) {
-    //         console.log(err);
-    //         return res.status(StatusCodes.BAD_REQUEST).end();
-    //     }
 
-    //     delivery_id = results.insertId;
+    let [results] = await conn.query(sql, values);
 
-    //     return res.status(StatusCodes.OK).json(results);
-    // });
+    console.log(results);
 
-    sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?,?,?,?,?)`;
-    values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+    // sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?,?,?,?,?)`;
+    // values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+
     // conn.query(sql, values, (err, results) => {
     //     if (err) {
     //         console.log(err);
@@ -36,28 +41,26 @@ const order = (req, res) => {
     //     }
 
     //     order_id = results.insertId;
-    //     console.log(order_id);
 
     //     return res.status(StatusCodes.OK).json(results);
     // });
 
-    sql = `INSERT INTO orderedBook (order_id, book_id, quantity)
-           VALUES ?;`;
+    // sql = `INSERT INTO orderedBook (order_id, book_id, quantity)
+    //        VALUES ?;`;
 
-    // items.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서)
-    values = [];
-    items.forEach((item) => {
-        values.push([order_id, item.book_id, item.quantity]);
-        console.log(values);
-    });
-    conn.query(sql, [values], (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
+    // // items.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서)
+    // values = [];
+    // items.forEach((item) => {
+    //     values.push([order_id, item.book_id, item.quantity]);
+    // });
+    // conn.query(sql, [values], (err, results) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return res.status(StatusCodes.BAD_REQUEST).end();
+    //     }
 
-        return res.status(StatusCodes.OK).json(results);
-    });
+    //     return res.status(StatusCodes.OK).json(results);
+    // });
 };
 const getOrders = (req, res) => {
     res.json('주문 목록 조회');
