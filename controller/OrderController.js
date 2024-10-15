c6922f1d70c75ae6afef2dc74f56e37549c28f88
leechan <dlcks0601@gmail.com>
2024-10-15 21:03:46 +0900

    10.15 주문api

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,10 +1,9 @@
-// const conn = require('../mariadb'); // db 모듈
-const mariadb = require('mysql2/promise');
-const { StatusCodes } = require('http-status-codes'); // status code 모듈
+const mysql = require('mysql2/promise');
+const { StatusCodes } = require('http-status-codes');
 
-const order = async (req, res) => {
-    const conn = await mariadb.createConnection({
-        host: '127.0.0.1',
+exports.order = async (req, res) => {
+    const conn = await mysql.createConnection({
+        host: 'localhost',
         user: 'root',
         password: 'root',
         database: 'Bookshop',
@@ -20,58 +19,74 @@ const order = async (req, res) => {
         firstBookTitle,
     } = req.body;
 
-    let delivery_id;
-    let order_id;
-
-    let sql =
-        'INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)';
+    let sql = `INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)`;
     let values = [delivery.address, delivery.receiver, delivery.contact];
+    let [results] = await conn.execute(sql, values);
+    let delivery_id = results.insertId;
 
-    let [results] = await conn.query(sql, values);
+    sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id)
+            VALUES (?, ?, ?, ?, ?)`;
+    values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+    [results] = await conn.execute(sql, values);
+    let order_id = results.insertId;
 
-    console.log(results);
+    sql = `SELECT book_id, quantity FROM cartItems WHERE id IN (?)`;
+    let [orderItems, fields] = await conn.query(sql, [items]);
 
-    // sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?,?,?,?,?)`;
-    // values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+    sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
 
-    // conn.query(sql, values, (err, results) => {
-    //     if (err) {
-    //         console.log(err);
-    //         return res.status(StatusCodes.BAD_REQUEST).end();
-    //     }
+    values = [];
+    orderItems.forEach((item) => {
+        values.push([order_id, items.book_id, items.quantity]);
+    });
 
-    //     order_id = results.insertId;
+    results = await conn.query(sql, [values]);
 
-    //     return res.status(StatusCodes.OK).json(results);
-    // });
-
-    // sql = `INSERT INTO orderedBook (order_id, book_id, quantity)
-    //        VALUES ?;`;
-
-    // // items.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서)
-    // values = [];
-    // items.forEach((item) => {
-    //     values.push([order_id, item.book_id, item.quantity]);
-    // });
-    // conn.query(sql, [values], (err, results) => {
-    //     if (err) {
-    //         console.log(err);
-    //         return res.status(StatusCodes.BAD_REQUEST).end();
-    //     }
-
-    //     return res.status(StatusCodes.OK).json(results);
-    // });
-};
-const getOrders = (req, res) => {
-    res.json('주문 목록 조회');
+    let result = await deleteCartItems(conn, items);
+    return res.status(StatusCodes.OK).json(result);
 };
 
-const getOrderDetail = (req, res) => {
-    res.json('주문 상세 상품 조회');
+const deleteCartItems = async (conn, items) => {
+    let sql = `DELETE FROM cartItems WHERE id IN (?)`;
+
+    let result = await conn.query(sql, [items]);
+    return result;
 };
 
-module.exports = {
-    order,
-    getOrders,
-    getOrderDetail,
+exports.getOrders = async (req, res) => {
+    const conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'Bookshop',
+        dateStrings: true,
+    });
+
+    let sql = `SELECT orders.id, created_at, address, receiver, contact, book_title, 
+            total_quantity, total_price
+            FROM orders LEFT JOIN delivery
+            ON orders.delivery_id = delivery.id`;
+
+    let [rows, fields] = await conn.query(sql);
+    return res.status(StatusCodes.OK).json(rows);
+};
+
+exports.getOrderDeteail = async (req, res) => {
+    const { id } = req.params;
+
+    const conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'Bookshop',
+        dateStrings: true,
+    });
+
+    let sql = `SELECT book_id, title, author, price, quantity 
+            FROM orderedBook LEFT JOIN books
+            ON orderedBook.book_id = books.id
+            WHERE order_id=?`;
+
+    let [rows, fields] = await conn.query(sql, [id]);
+    return res.status(StatusCodes.OK).json(rows);
 };
