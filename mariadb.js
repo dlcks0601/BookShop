fa5dcd8c6cf0837f480f0ce31f99cce2771dfd43
leechan <dlcks0601@gmail.com>
2024-10-16 19:27:42 +0900

    order 수정

--- mariadb.js
+++ mariadb.js
@@ -1,17 +1,11 @@
 // mysql 모듈 소환
-const mariadb = require('mysql2/promise');
+const mariadb = require('mysql2');
 
-// DB와 연결 통로 생성
-const connection = async () => {
-    const conn = await mariadb.createConnection({
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'root',
-        database: 'Bookshop',
-        dateStrings: true,
-    });
-
-    return conn;
-};
-
-module.exports = connection;
+const connection = mariadb.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'Bookshop',
+    dateStrings: true,
+});
+return connection;
--- routes/orders.js
+++ routes/orders.js
@@ -5,11 +5,14 @@ const {
     getOrderDetail,
 } = require('../controller/OrderController');
 const router = express.Router();
-
+//post 형태로 데이터를 받으려면 json형태로 받게끔 도와주는 모듈
 router.use(express.json());
+//주문하기
+router.post('/', order);
+//주문 목록 조회
+router.get('/', getOrders);
 
-router.post('/', order); // 주문 하기
-router.get('/', getOrders); // 주문 목록 조회
-router.get('/:id', getOrderDetail); // 주문 상세 조회
+//주문 상세 상품 조회
+router.get('/:id', getOrderDetail);
 
 module.exports = router;
